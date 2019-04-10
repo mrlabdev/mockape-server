b7a9fab1ee036c9422935abb86f3c1d1c6f5ccac
Shelton Han <shelton.han@sbs.com.au>
2019-04-11 00:02:34 +1000

    Add CORS, listen to port 333; add new mock data

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ var mockFileListRouter = require("./routes/mockfiles");
 
 var app = express();
 
+// Enable CORS
+var allowCrossDomain = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+};
+app.use(allowCrossDomain);
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
